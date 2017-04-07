a7d002fa89cb05e4f2b1c23733959e4fa7c4e634
harry <harry.feng@tangramtek.com>
2017-04-07 16:14:48 +0800

    inline asm tests

---
+++ asm_test.cpp
@@ -0,0 +1,28 @@
+/*
+ * asm_test.cpp
+ *
+ *  Created on: Apr 4, 2017
+ *      Author: harry
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10, b = 20;
+
+	// output a
+	cout << "a=" << a << "\n";
+	asm("mov $5, %eax;");
+	asm("mov %%eax, %0;" : "=r"(a));
+	cout << "a=" << a << "\n";
+
+	// input b
+	asm("mov %0, %%eax;" :: "r"(b));
+	asm("sub $5, %eax;");
+	asm("mov %%eax, %0;" : "=r"(a));
+	cout << "a=" << a << "\n";
+
+	return 0;
+}
---
+++ lib1.cpp
@@ -0,0 +1,16 @@
+/*
+ * lib1.cpp
+ *
+ *  Created on: Apr 1, 2017
+ *      Author: harry
+ */
+
+#include <iostream>
+using namespace std;
+
+void lib1_func1()
+{
+    cout << "lib1_func1 called \n";
+}
+
+
