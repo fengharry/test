7464f9de91d5d8bf6fb5f03e404a96f66bdcb5da
harry <harry.feng@tangramtek.com>
2017-05-05 17:47:37 +0800

    windows

--- aesenc.cpp
+++ aesenc.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+#if defined(os_windows)
+#define ASMs(op, src, dest) __asm op dest, src
+#define ASMd(op, src, dest) asm(#op src, %0 :: "m"(dest))
+#endif
+
+#if defined(os_linux)
+#define ASMs(op, src, dest) asm("op %0, dest" :: "m"(src))
+#define ASMd(op, src, dest) asm("op src, %0" :: "m"(dest))
+#endif
+
 int main()
 {
 	unsigned char input[16], key[16], output[16];
@@ -9,10 +19,12 @@ int main()
 		input[i] = key[i] = output[i] = i;
 	}
 
-	asm("movdqa %0, %%xmm0" :: "m"(input));
-	asm("movdqa %0, %%xmm1" :: "m"(key));
-	asm("aesenc %xmm0, %xmm1");
-	asm("movdqa %%xmm1, %0" : "=m"(output));
+	//asm("movdqa %0, %%xmm0" :: "m"(input));
+	ASMd(movdqa, input, xmm0);
+	__asm movdqa input, xmm0;
+	//asm("movdqa %0, %%xmm1" :: "m"(key));
+	//asm("aesenc %xmm0, %xmm1");
+	//asm("movdqa %%xmm1, %0" : "=m"(output));
 
 	for(int i=0;i<16;i++)
 	{
