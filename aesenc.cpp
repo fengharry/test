51cb155d91a7f834fb3d1d9f815d95d65663f1ab
harry <fengharry126@126.com>
2017-05-06 14:27:26 +0800

    work in both windows and linux

--- aesenc.cpp
+++ aesenc.cpp
@@ -2,30 +2,41 @@
 using namespace std;
 
 #if defined(os_windows)
+// src is in memory, dest is register
 #define ASMs(op, src, dest) __asm op dest, src
-#define ASMd(op, src, dest) asm(#op src, %0 :: "m"(dest))
+// src is register, dest is in memory
+#define ASMd(op, src, dest) __asm op dest, src
+// both src and dest are in register
+#define ASMn(op, src, dest) __asm op dest, src
+
+#define ALIGN(X) __declspec(align(X))
 #endif
 
 #if defined(os_linux)
-#define ASMs(op, src, dest) asm("op %0, dest" :: "m"(src))
-#define ASMd(op, src, dest) asm("op src, %0" :: "m"(dest))
+#define COMMA ,
+#define QUOTE(X) #X
+#define ADD_QUOTE(X) QUOTE(X)
+#define ASMs(op, src, dest) asm(ADD_QUOTE(op %0 COMMA %%dest) :: "m"(src))
+#define ASMd(op, src, dest) asm(ADD_QUOTE(op %%src COMMA %0) :: "m"(dest))
+#define ASMn(op, src, dest) asm(ADD_QUOTE(op %src COMMA %dest))
+
+#define ALIGN(X)
 #endif
 
 int main()
 {
-	unsigned char input[16], key[16], output[16];
+	ALIGN(16) unsigned char input[16], key[16], output[16];
 	for(int i=0;i<16;i++)
 	{
 		input[i] = key[i] = output[i] = i;
 	}
 
-	//asm("movdqa %0, %%xmm0" :: "m"(input));
-	ASMd(movdqa, input, xmm0);
-	__asm movdqa input, xmm0;
-	//asm("movdqa %0, %%xmm1" :: "m"(key));
-	//asm("aesenc %xmm0, %xmm1");
-	//asm("movdqa %%xmm1, %0" : "=m"(output));
+	ASMs(movdqa, input, xmm0);
+	ASMs(movdqa, key, xmm1);
+	ASMn(aesenc, xmm1, xmm0);
+	ASMd(movdqa, xmm0, output);
 
+	// 6a 6b 5e 46 28 68 35 56 b8 d0 57 6a 2b 91 2f 53
 	for(int i=0;i<16;i++)
 	{
 		cout << hex << (int)output[i] << " ";
