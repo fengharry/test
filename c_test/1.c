6a310bb0b9f432dc0dfcfa854e5d59a077426502
harry <fengharry126@126.com>
2018-08-05 11:38:56 +0800

    tests using c

---
+++ c_test/1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int test1()
+{
+	int a = 0;
+    printf("hello world\n");
+	printf("a=0x%x\n", a);
+	return 0;
+}
---
+++ c_test/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	test1();
+	return 0;
+}
